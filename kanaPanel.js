228e48a12dabffb4a5b44182a951959347700171
jd <jdwebdev974@gmail.com>
2021-09-05 15:11:27 +0200

    Add new classes kanaBtn & kanaPanel

---
+++ kanaPanel.js
@@ -0,0 +1,5 @@
+class KanaPanel extends Panel {
+    constructor(pSize, pX, pY, pCallback, pType = "normal", pTypeState = null, pLabel = "", pId = 0, pStaticSize = false) {
+        super(pSize, pX, pY, pCallback, pType, pTypeState, pLabel, pId, pStaticSize);
+    }
+}
